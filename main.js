231b922dfa8adc9890ddea6b5bb5c398d31e1d0a
Kauane dos Santos Michalewicz <125034991+michalewicz07@users.noreply.github.com>
2024-04-19 08:57:52 -0300

    alt2

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual os melhor time?");
+    var respostaTime = prompt("Qual o melhor o colégio público de Arapongas?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "A") {
+      alert("Isso mesmo! O Garcez!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
